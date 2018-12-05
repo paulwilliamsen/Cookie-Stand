001a2dd4d60d93bdc13cc86b5fd21650de387aff
Paul Williamsen <pwilliamsen@me.com>
2018-12-05 12:28:15 -0800

    New commit

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ Store.prototype.cookiesPerCustomer = function () {
     var singleHourCookies = Math.floor(this.numCustHourly() * this.avg);
     this.cookiesEachHour.push(singleHourCookies);
     this.totalCookies += singleHourCookies;
-     
+
   }
 };
 
@@ -75,35 +75,36 @@ var capitalHill = new Store('Capital Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
 // Still working on the footer//
-// function footerRow() {
-//   var tableRow = document.createElement('tr');
-//   var thEl = document.createElement('td');
-//   thEl.textContent = 'Hourly Total';
-//   tableRow.appendChild(thEl);
+function footerRow() {
+  var tableRow = document.createElement('tr');
+  var thEl = document.createElement('td');
+  thEl.textContent = 'Hourly Total';
+  tableRow.appendChild(thEl);
+  cookieTable.appendChild(tableRow);
 
-//   var allTotals = 0;
-//   for (var i = 0; i < hours.length; i++) {
-//     var hourlyTotal = 0;
-//     console.log(cookieTable.rows[i]);
-//     for (var x = 0; x < hours.length; x++) {
-//       hourlyTotal = hourlyTotal + cookieTable[i].totalCookies[x];
-//       allTotals += cookieTable[x].totalCookies[i];
-//       console.log(hourlyTotal);
-//     }
-//     var tdElement = document.createElement('td');
-//     tdElement.textContent = hourlyTotal;
-//     tableRow.appendChild(tdElement);
-//   }
-//   tdElement = document.createElement('td');
-//   tdElement.textContent = allTotals;
-//   tableRow.appendChild(tdElement);
-// }
+  var allTotals = 0;
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyTotal = 0;
+    console.log(cookieTable.rows[i]);
+    for (var x = 0; x < hours.length; x++) {
+      hourlyTotal = hourlyTotal + cookieTable[i].totalCookies[x];
+      allTotals += cookieTable[x].totalCookies[i];
+      console.log(hourlyTotal);
+    }
+    var tdElement = document.createElement('td');
+    tdElement.textContent = hourlyTotal;
+    tableRow.appendChild(tdElement);
+  }
+  tdElement = document.createElement('td');
+  tdElement.textContent = allTotals;
+  tableRow.appendChild(tdElement);
+}
 
 makeHeaderRow();
+alki.render();
+capitalHill.render();
 firstandPike.render();
 seattleAirport.render();
 seattleCenter.render();
-capitalHill.render();
-alki.render();
-// footerRow();
+footerRow();
 
